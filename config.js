0111e03350d0d9150a76fe5cd8389585920dbaa3
AshleyCanty <AshleyCanty@users.noreply.github.com>
2018-08-05 11:33:29 -0400

    some changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   //https://developers.themoviedb.org/3/getting-started/introduction
   // check documentation
   apiActorURL: 'https://api.themoviedb.org/3/person/',
-  apiImageURL: 'https://image.tmdb.org/t/p/original',
+  apiImageURL: 'http://image.tmdb.org/t/p/original',
   apiGenreID: 'https://api.themoviedb.org/3/genre/movie/list?api_key=f20135b292080c1a6bdcdd3cf8616d48',
   apiURL:'https://api.themoviedb.org/3',
   apiKey: '?api_key=f20135b292080c1a6bdcdd3cf8616d48'
